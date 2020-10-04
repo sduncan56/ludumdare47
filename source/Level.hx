db7fc63cfc864a9c345ed7b0e0d42e6e549be7bc
Shane <shane.duncan56@gmail.com>
2020-10-04 14:37:14 +1000

    add files

---
+++ source/Level.hx
@@ -0,0 +1,30 @@
+import flixel.addons.editors.ogmo.FlxOgmo3Loader;
+import flixel.tile.FlxTilemap;
+import flixel.math.FlxPoint;
+
+
+
+class Level
+{
+    public var name(default, default):String;
+    public var startPos(default, default):FlxPoint;
+    public var loopsCompleted(default, default):Int = 0;
+    public var loopsRequired(default, default):Int;
+    public var levelNumber(default, default):Int;
+
+    public var walls(default, default):FlxTilemap;
+    public var base(default, default):FlxTilemap;
+    public var ogmoData(default, default):FlxOgmo3Loader;
+    public var width(default, default):Float;
+    public var height(default, default):Float;
+    public var loopReady(default, default):Bool = false;
+
+    public var loopedBase(default, default):FlxTilemap = null;
+    public var loopedWalls(default, default):FlxTilemap = null;
+
+    public function new()
+    {
+
+    }
+
+}
---
+++ source/entities/LoopController.hx
@@ -0,0 +1,9 @@
+package entities;
+
+class LoopController extends Entity
+{
+    public function new(x:Float, y:Float, frameName:String) {
+        super(x, y, false, frameName);
+
+    }
+}
